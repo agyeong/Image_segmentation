13a7180f5b3c1eb99f4aa2601af8ec43748fab97
EP000 <bzxz55@gmail.com>
2021-06-01 00:30:31 +0900

    Upload DL3 Simple version 1

---
+++ Semantic-Segmentation-Simple-ver1/README.md
@@ -0,0 +1,9 @@
+# DeepLap v3 
+### Description
+- semantic image segmentation
+- ref : https://github.com/hoya012/semantic-segmentation-tutorial-pytorch
+### Files
+- helpers.py : For calculation of iou, acc, loss, etc.
+- labels.py : About pixel and label values in data.
+- model.py : Define Model Structure.
+- RUN.ipynb : Files that you run
---
+++ Semantic-Segmentation-Simple-ver1/labels.py
@@ -0,0 +1,42 @@
+from collections import namedtuple
+
+Label = namedtuple( 'Label' , ['name', 'id', 'color'] )
+
+labels = [
+    #       name                     Id       color
+    
+    Label(  'Pole'                 , 0       ,   (  0,  0, 64)     ),
+    Label(  'Pole'                 , 0       ,   (192,192,128)     ),
+    Label(  'SignSymbol'           , 1       ,   (128,128, 64)     ),
+    Label(  'SignSymbol'           , 1       ,   (192,128,128)     ),
+    Label(  'SignSymbol'           , 1       ,   (  0, 64, 64)     ),
+    
+    Label(  'Bicyclist'            , 2       ,   (  0,128,192)     ),
+    Label(  'Bicyclist'            , 2       ,   (192,  0,192)     ),
+    Label(  'Pedestrian'           , 3       ,   ( 64, 64,  0)     ),
+    Label(  'Pedestrian'           , 3       ,   (192,128, 64)     ),
+    Label(  'Pedestrian'           , 3       ,   ( 64,128, 64)     ),
+    Label(  'Pedestrian'           , 3       ,   ( 64,  0,192)     ),
+    
+    Label(  'Building'             , 4       ,   ( 64,  0, 64)     ),
+    Label(  'Building'             , 4       ,   (128,  0,  0)     ),
+    Label(  'Building'             , 4       ,   (192,  0,128)     ),
+    Label(  'Building'             , 4       ,   ( 64,192,  0)     ),
+    Label(  'Building'             , 4       ,   (  0,128, 64)     ),
+    Label(  'Fence'                , 5       ,   ( 64,128, 64)     ),
+    
+    Label(  'Pavement'             , 6       ,   ( 64,192,128)     ),
+    Label(  'Pavement'             , 6       ,   (128,128,192)     ),
+    Label(  'Pavement'             , 6       ,   (  0,  0,192)     ),
+    Label(  'Road'                 , 7       ,   (192,  0, 64)     ),
+    
+    Label(  'Car'                  , 8       ,   ( 64,128,192)     ),
+    Label(  'Car'                  , 8       ,   (128, 64, 64)     ),
+    Label(  'Car'                  , 8       ,   (192,128,192)     ),
+    Label(  'Car'                  , 8       ,   ( 64,  0,128)     ),
+    
+    Label(  'Sky'                  , 9       ,   (128,128,128)     ),
+    
+    Label(  'Tree'                 , 10      ,   (192,192,  0)     ),
+    Label(  'Tree'                 , 10      ,   (128,128,  0)     ),
+]
